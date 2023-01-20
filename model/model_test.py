9869d7a9be8db5eae55392dfd0c768c997db779c
woodong11 <91379630+woodong11@users.noreply.github.com>
2023-01-20 15:04:43 +0900

    Add files via upload

---
+++ model/model_test.py
@@ -0,0 +1,116 @@
+
+# -*- coding: utf-8 -*-
+import cv2 as cv
+import time
+import numpy as np
+from matplotlib import pyplot as plt
+from keras.models import load_model
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator
+
+
+CAM_ID = 0      # camid 설정
+
+
+'''opencv로 화면 출력 저장, resize하기 캡쳐 함수'''
+def capture(camid = CAM_ID):
+    start = time.time()
+    #cam = cv.VideoCapture(camid, cv.CoAP_DSHOW)            #현재 컴퓨터에 연결된 메인 카메라 불러오기      
+    cam = cv.VideoCapture(camid)
+    if cam.isOpened() == False:
+        print ('can''t open the cam (%d)' % camid)    # 캠 연결 불가때
+        return None
+
+    ret, img = cam.read()                   # ret: 읽음 여부, img: 이미지 파일
+    if ret == 0:                            # 읽음 실패시 프린트하기.
+        print ('frame is not exist')
+        return None
+    resize_img = cv.resize(img, (24,24))    #24 x 24로 사이즈 줄이기
+    resize_img_gray = cv.cvtColor(resize_img, cv.COLOR_BGR2GRAY)
+    cv.imwrite('test.png',img, params=[cv.IMWRITE_PNG_COMPRESSION,0])
+    
+    
+    # 사진 배경 흰색으로 만들기 위한 2중 for문 -> 배경 강제로 하얗게 만들기
+    for i in range(24):         
+        for j in range(24):  
+            if resize_img_gray[i][j] > 130:         
+                resize_img_gray[i][j] = 255     
+                                    
+    #print(resize_img_gray)                 # 출력 테스트
+    cv.imwrite('test2.png',resize_img_gray, params=[cv.IMWRITE_PNG_COMPRESSION,0])   # 사진 저장
+    #cv.imshow("resize_img", img)
+    end = time.time()
+    #plt.imshow(resize_img_gray, cmap='gray')   # matplotlib으로 화면 송출, 0일수록 검정, 255일수록 하양
+    #plt.show()
+
+    cam.release()   # 카메라 동적할당 해제
+
+    print("opencv time: %f sec" %(end - start))
+    return resize_img
+    
+
+               
+
+  
+''' main '''
+
+model = load_model('lig_model.h5')  # 모델 불러오기
+
+# while문 돌리기
+print("################# model start #######################")
+while(True):
+    #i = 1
+    print("press anykey to start, press q to quit \n")
+    key_input = input()
+    
+    if key_input == 'q':        # q 입력시 종료
+        break
+    
+    else: 
+        #current_image = tf.keras.utils.load_img('warehouse/handwriting_shape/test/test.png')       # 케라스로 이미지 로드하기
+        img = capture(CAM_ID)          # 캡쳐함수 실행
+        
+        #current_img  = cv.imread(current_img)                       # opencv로 이미지 로드하기
+        #input_arr = tf.keras.utils.img_to_array(current_img)                                       # 이미지 넘파이 배열로 바꾸기? 근데 필요없음.
+        input_arr = np.array([img])  # Convert single image to a batch.
+        pred = model.predict(input_arr)                                             # 예측하기
+        #np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})    # x당 0.3f씩만 -> 있어도 되고 없어도 되는 줄
+
+        classes = ['circle', 'square', 'triangle']        # 라벨들
+
+        ''' 출력   '''
+        #print(pred)                                    # 인덱스만 출력하고 싶을때 이 줄 사용
+        where = np.where(pred == pred.max())[1][0]      # 넘파이는 인덱스찾는 함수가 numpy.where이다.
+        print("###################")
+        print(pred)
+        print("pattern: " + classes[where])                   
+        print("###################\n\n")
+        plt.imshow(img, cmap='gray')                    # matplotlib으로 화면 송출, 0일수록 검정, 255일수록 하양
+        plt.show()
+    #i += 1
+
+   
+cv.destroyAllWindows()  # 종료
+
+
+
+# cap = cv.VideoCapture(0)    #현재 컴퓨터에 연결된 메인 카메라 return
+# if not cap.isOpened():
+#     print("camera open failed")
+#     exit()                      # 카메라가 연결되어 있지 않으면 종료
+
+# ret, img = cap.read()
+# if not ret:                     
+#     print("Can't read camera")  # 이미지 읽기 실패했을 경우 
+    
+# cv.imshow('test', img)          # 이미지 윈도우창에 띄우기
+
+# if cv.waitKey(1) == ord('c'):   # 키보드 값이 c면 이미지 저장
+#     img_captured = cv.imwrite('img_captured.png', img)
+# #elif cv.waitKey(1) == ord('q'):       # 입력에 q가 들어오면 종료 
+    
---
+++ model/train.py
@@ -0,0 +1,101 @@
+# 0. 사용할 패키지 불러오기
+import numpy as np
+import cv2 as cv
+import keras
+from keras.models import Sequential
+from keras.models import load_model
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.preprocessing.image import ImageDataGenerator
+
+
+# 랜덤시드 고정시키기
+np.random.seed(3)
+
+
+# 1. 데이터 생성하기
+train_datagen = ImageDataGenerator(rescale=1./255)  # 클래스로 객체 생성
+# 훈련 제너레이터 생성
+train_generator = train_datagen.flow_from_directory(    
+        'warehouse/handwriting_shape/train',            # 인풋 이미지 경로 지정
+        target_size=(24, 24),                           # 패치 이미지 크기 지정
+        batch_size=3,                                   # 배치 크기 지정
+        class_mode='categorical')                       # 분류 방식 지정 -> categorical: 2D one-hot 부호화된 라벨 반환 
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+# 테스트 제너레이터 생성
+test_generator = test_datagen.flow_from_directory(       
+        'warehouse/handwriting_shape/test',             
+        target_size=(24, 24),    
+        batch_size=3,
+        shuffle = False,                     # 섞지 않는다
+        class_mode='categorical')
+
+
+# 2. 케라스 모델 만들기
+#2-1 모델 생성하기(층 생성)
+model = Sequential()            # 제일 처음으로 받아오는 층: Sequential
+model.add(Conv2D(32, kernel_size=(3, 3),    # 필터크기는 3, 활성화함수는 relu
+                 activation='relu',
+                 input_shape=(24,24,3)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))   # 풀링 크기는 2x2
+model.add(Flatten())                        # Flaten층            
+model.add(Dense(128, activation='relu'))
+model.add(Dense(3, activation='softmax'))       
+
+
+#2-2 케라스 모델 학습과정 설정하기 
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+# loss: 사용 손실함수, 다중 클래스 문제이므로 저렇게 설정함. optimizer: 가중치 검색 최적화, 효과적인 경사하강법 알고리즘 중 adam 사용
+# metrics: 평가척도 나타냄
+
+
+# 2-3 모델 훈련
+model.fit_generator(
+        train_generator,    #훈련 데이터셋 제공할 제너레이터 지정. 앞에 있던 train_generator 지정
+        steps_per_epoch=15, # 한 에포크에 사용할 스텝 수 지정. 45개 훈련샘플, 배치사이즈 3이므로 15스텝 지정
+        epochs=50,          # 학습 반복 횟수
+        validation_data=test_generator, # 검증데이터셋 제공할 제너레이터 지정
+        validation_steps=5)             # 한 에포크 종료마다 검증할때 사용되는 검증 스텝 수 지정. 15개 검증샘플, 배치사이즈 3 -> 5스텝
+
+
+# 2-4 모델 평가
+print("######## Evaluate ########")
+scores = model.evaluate_generator(test_generator, steps=5)
+print("%s: %.2f%%" %(model.metrics_names[1], scores[1]*100))
+
+# 2-5 모델 저장
+model.save('lig_model.h5')
+
+#모델 사용하기
+# print("######### Predict ###########")
+# #current_image = tf.keras.utils.load_img('warehouse/handwriting_shape/test/test.png')       # 케라스로 이미지 로드하기
+# current_img  = cv.imread('warehouse/handwriting_shape/test/test1.png')                       # opencv로 이미지 로드하기
+# #input_arr = tf.keras.utils.img_to_array(current_img)                                       # 이미지 넘파이 배열로 바꾸기? 근데 필요없음.
+# input_arr = np.array([current_img])  # Convert single image to a batch.
+# pred = model.predict(input_arr)                                             # 예측하기
+# #np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})    # x당 0.3f씩만 -> 있어도 되고 없어도 되는 줄
+
+# classes = ['circle', 'triangle', 'squre']       # 라벨들
+# print(pred)
+# # 출력
+# #print() # 인덱스만 출력하고 싶을때
+# #print(np.where(pred == pred.max()))                   # 넘파이는 인덱스찾는 함수가 numpy.where이다.
+
+
+
+
+
+# 제너레이터에서 출력되는걸로 생성하기
+# 여러 이미지 데이터 불러올땐 제너레이터로 만든다. 
+output = model.predict_generator(test_generator, steps=5)
+np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})    # x당 0.3f씩만    
+print(test_generator.class_indices)
+print(output)
+
+print('\n'.join(test_generator.filename))   #파일 이름을 엔터키 순서대로 출력해주는 코드
+
+
